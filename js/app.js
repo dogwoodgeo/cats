3b2187e803abd8282264c9153161a04387467ae5
Brad Jones <bradjones@Smaug.local>
2019-11-25 10:34:43 -0600

    Made some edits to the text content and styling.

--- js/app.js
+++ js/app.js
@@ -69,13 +69,14 @@ function(
   //* Info panel expand widget
   const expandContent = '<h3 style="color: #a78176">Cat Sightings</h3>' + 
                         '<span style="color: #b2495a">' + 
-                        'This is a fun side project I developed to track the cats I observe and interact with while living my life. Most located in Central Arkansas, but with other regions included as I travel<br><br>' + 
+                        'This is a fun side project I developed to track the cats I observe and interact with while living my life. Most are located in Central Arkansas, but with other regions included as I travel.<br><br>' + 
                         'I use this project to experiment with aspects of web development. Mostly relating to WebGIS and the ArcGIS API for Javascript.' +
                         '<br><br>Most of the cats are observed by me (Bradley Jones), but some data may have been collected by my daughter, Maggie, or my wife, Kelli.<br><br></span>' + 
                         '<span style="color: #a78176 ; font-weight: 900">Contact</span><br>' +
-                        '<span style="color: #b2495a">Bradley Jones</span><br>' + 
+                        '<span style="color: #b2495a">Bradley Jones</span><br>' +
+                        '<span style="color: #b2495a; font-style: italic">Dogwood Geospatial Services</span><br>' + 
                         '<a href="mailto:webmaster@example.com">bjones@dogwoodgeo.com</a><br>' + 
-                        '<a href="https://github.com/dogwoodgeo/cats">Source Code</a><br>' 
+                        '<a href="https://github.com/dogwoodgeo/cats" target="_blank">Source Code</a><br>' 
                         
 
   infoExpand = new Expand({
